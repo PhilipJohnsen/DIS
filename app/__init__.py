671dda01f4f38cb1b5c967db53ea559bf7dbf612
zjt716 <antonbaek4@gmail.com>
2025-05-28 10:54:31 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,11 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecret'
 
-# DATABASE CONNECTION (SQLite -> psycopg2 needs PostgreSQL!)
+# CAll the Database Zalandis, user: postgres, password: use your own PG password, host: localhost, port: 5432
 conn = psycopg2.connect(
-    dbname='Zalandis',
+    dbname='Zalandis', # Make sure to create the database in PostgreSQL
     user='postgres',
-    password='Sqngfn3a',
+    password='Sqngfn3a', # Write your own password
     host='localhost',
     port='5432'
 )
